925fc410bbf031a0236d2d189947c4d5560dcf61
nanayawisrael <nanayawisrael@gmail.com>
2024-10-29 19:12:47 +0000

    Divider top and bottom margin reduced drastically

--- src/components/Divider/Divider.jsx
+++ src/components/Divider/Divider.jsx
@@ -1,7 +1,7 @@
 const Divider = () => {
     return (
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="my-16 md:my-24">
+        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
+            <div className="my-4">
                 <hr className="border-t border-gray-200" />
             </div>
         </div>
