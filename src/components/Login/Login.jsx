d6ee760f0586fc798a356786aa65d08e0ed92f0a
nanayawisrael <nanayawisrael@gmail.com>
2024-10-29 13:09:16 +0000

    Updating Login component to use Tailwind CSS. Vanilla CSS styling commented out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './Login.module.css';
+//import styles from './Login.module.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
