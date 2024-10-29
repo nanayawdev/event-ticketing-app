818adc19c8fe528f4eff0257576519f43ee621df
nanayawisrael <nanayawisrael@gmail.com>
2024-10-29 17:15:29 +0000

    Footer added to the About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Features from '../Features/Features';
 import Stats from '../Stats/Stats';
 import Brands from '../Brands/Brands';
+import Footer from '../Footer/Footer';
 
 export default function About() {
   return (
@@ -9,6 +10,7 @@ export default function About() {
       <Features />
       <Stats />
       <Brands />
+      <Footer />
     </>
   )
 }
