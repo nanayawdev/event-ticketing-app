632fff2b724c86d948448c19d198bdf3fd79235b
nanayawisrael <nanayawisrael@gmail.com>
2024-10-29 14:15:56 +0000

    Glow speed reduced from 3s to 8s

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,16 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        gradient: 'gradient 3s ease infinite',
+        gradient: 'gradient 8s ease infinite',
       },
       keyframes: {
         gradient: {
           '0%, 100%': {
-            'background-size': '200% 200%',
+            'background-size': '300% 300%',
             'background-position': 'left center'
           },
           '50%': {
-            'background-size': '200% 200%',
+            'background-size': '300% 300%',
             'background-position': 'right center'
           },
         },
