186204e6b65cd8db8c80928c095470fc424851d2
nanayawisrael <nanayawisrael@gmail.com>
2024-11-01 15:24:45 +0000

    Still finding the best layout for Our Story component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import OurStory from '../OurStory/OurStory';
 import Features from '../Features/Features';
 import Stats from '../Stats/Stats';
 import Brands from '../Brands/Brands';
@@ -8,7 +7,6 @@ import Footer from '../Footer/Footer';
 export default function About() {
   return (
     <>
-      <OurStory />
       <Features />
       <Stats />
       <Brands />
