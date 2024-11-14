b8d84d70ed890dd3d98b68a211dbec241063712a
nanayawisrael <nanayawisrael@gmail.com>
2024-11-14 17:43:00 +0000

    Dark Mode added to Features component

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -30,16 +30,16 @@ const features = [
 
 export default function Features() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white dark:bg-gray-900 py-24 sm:py-32">
       <div className="mx-auto max-w-[1200px] px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="inline-block text-base font-normal leading-7 text-sea-green-600 border border-sea-green-600 rounded-md px-4 py-1">
+          <h2 className="inline-block text-base font-normal leading-7 text-primary-600 dark:text-primary-400 border border-primary-600 dark:border-primary-400 rounded-md px-4 py-1">
             Event simplified
           </h2>
           <h1 className="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">
             Everything you need to host a successful event
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600 mx-auto max-w-xl">
+          <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400 mx-auto max-w-xl">
             Powering seamless event experiences for thousands of creators, online and offline, since 2013
           </p>
         </div>
@@ -47,13 +47,13 @@ export default function Features() {
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-normal leading-7 text-gray-900">
-                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-md bg-sea-green-400">
-                    <feature.icon aria-hidden="true" className="h-6 w-6 text-white" />
+                <dt className="text-base font-normal leading-7 text-gray-900 dark:text-gray-50">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-md bg-primary-600 dark:bg-primary-400">
+                    <feature.icon aria-hidden="true" className="h-6 w-6 text-white dark:text-gray-50" />
                   </div>
                   {feature.name}
                 </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
+                <dd className="mt-2 text-base leading-7 text-gray-600 dark:text-gray-400">{feature.description}</dd>
               </div>
             ))}
           </dl>
