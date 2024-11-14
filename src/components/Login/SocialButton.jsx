6bb6c02af8d483bd8ce2a077fd4db1900ff0e4c9
nanayawisrael <nanayawisrael@gmail.com>
2024-11-14 13:51:05 +0000

    Added dark mode styles to the social button component

--- src/components/Login/SocialButton.jsx
+++ src/components/Login/SocialButton.jsx
@@ -7,7 +7,7 @@ const SocialButton = ({ icon: Icon, name, onClick }) => {
     <div className="relative inline-block">
       <button
         type="button"
-        className="w-full inline-flex items-center justify-center p-3 rounded-lg border border-gray-200 bg-white text-gray-500 hover:text-gray-700 transition-all duration-200 focus:outline-none"
+        className="w-full inline-flex items-center justify-center p-3 rounded-lg border border-gray-200 bg-white text-gray-500 hover:text-gray-700 transition-all duration-200 focus:outline-none dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
         onMouseEnter={() => setShowTooltip(true)}
         onMouseLeave={() => setShowTooltip(false)}
         onClick={onClick}
@@ -17,11 +17,11 @@ const SocialButton = ({ icon: Icon, name, onClick }) => {
       
       {/* Tooltip */}
       {showTooltip && (
-        <div className="absolute z-50 bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1.5 text-xs font-medium text-white bg-gray-900 rounded-md whitespace-nowrap">
+        <div className="absolute z-50 bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1.5 text-xs font-medium text-white bg-gray-900 rounded-md whitespace-nowrap dark:bg-gray-700">
           Sign up with {name}
           {/* Arrow */}
           <div className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-px">
-            <div className="w-0 h-0 border-4 border-transparent border-t-gray-900" />
+            <div className="w-0 h-0 border-4 border-transparent border-t-gray-900 dark:border-t-gray-700" />
           </div>
         </div>
       )}
