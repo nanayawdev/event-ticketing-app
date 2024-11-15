84533d29d14322f0a42ceabbd4472ff3fd05dab5
nanayawisrael <nanayawisrael@gmail.com>
2024-11-15 18:03:31 +0000

    CountriesData.js created to populates selected countries where the app is working or accessible

---
+++ src/components/PhoneSignIn/countriesData.js
@@ -0,0 +1,11 @@
+export const countries = [
+  { name: "United States", code: "US", dialCode: "+1" },
+  { name: "United Kingdom", code: "GB", dialCode: "+44" },
+  { name: "India", code: "IN", dialCode: "+91" },
+  { name: "Kenya", code: "KE", dialCode: "+254" },
+  { name: "Nigeria", code: "NG", dialCode: "+234" },
+  { name: "South Africa", code: "ZA", dialCode: "+27" },
+  { name: "Uganda", code: "UG", dialCode: "+256" },
+  { name: "Tanzania", code: "TZ", dialCode: "+255" },
+  // Add more countries as needed
+]; 
