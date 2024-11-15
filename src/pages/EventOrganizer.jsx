f6f62b3e5d59938d21be7faa0295e447df57cb9b
nanayawisrael <nanayawisrael@gmail.com>
2024-11-15 12:39:03 +0000

    OrgHero component deleted to make the Organizers' page more simple and attractive

--- src/pages/EventOrganizer.jsx
+++ src/pages/EventOrganizer.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import OrganizerFeatures from '../components/OrganizerFeatures/OrganizerFeatures';
 import OrganizerFeatures2 from '../components/OrganizerFeatures2/OrganizerFeatures2';
 import OrganizerFeatures3 from '../components/OrganizerFeatures3/OrganizerFeatures3';
-import OrgHero from '../components/OrgHero/OrgHero';
 export default function EventOrganizer() {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900">
-      <OrgHero />
       <OrganizerFeatures />
       <OrganizerFeatures2 />
       <OrganizerFeatures3 />
